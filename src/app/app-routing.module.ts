a52fc91a527f5e9fd8f4f9d31ecabce4d39a6982
pbajoao <joaopaulo.jpba2015@gmail.com>
2020-04-16 00:13:47 -0300

    SCJB-004 - Router Guards + Logout

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,32 +1,33 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { AuthenticationGuard, AnonymousGuard } from './core/guards';
 
 const routes: Routes = [
   {
-    path: '',
-    redirectTo: 'login',
-    pathMatch: 'full'
+    path: '', redirectTo: 'login', pathMatch: 'full'
   },
   {
     path: 'login',
-    loadChildren: () => import('./modules/login/login.module').then(m => m.LoginPageModule)
+    loadChildren: () => import('./modules/login/login.module').then(m => m.LoginPageModule),
+    canActivate: [AnonymousGuard]
   },
   {
     path: 'register',
-    loadChildren: () => import('./modules/register/register.module').then(m => m.RegisterPageModule)
+    loadChildren: () => import('./modules/register/register.module').then(m => m.RegisterPageModule),
+    canActivate: [AnonymousGuard]
   },
   {
     path: 'recover-password',
-    loadChildren: () => import('./modules/recover-password/recover-password.module').then(m => m.RecoverPasswordPageModule)
+    loadChildren: () => import('./modules/recover-password/recover-password.module').then(m => m.RecoverPasswordPageModule),
+    canActivate: [AnonymousGuard]
   },
   {
     path: 'tabs',
     loadChildren: () => import('./core/menu-header/menu-header.module').then(m => m.MenuHeaderPageModule),
+    canActivate: [AuthenticationGuard]
   },
   {
-    path: '**',
-    redirectTo: 'login',
-    pathMatch: 'full'
+    path: '**', redirectTo: 'login', pathMatch: 'full'
   },
 ];
 @NgModule({
